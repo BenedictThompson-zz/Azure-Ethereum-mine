82b9587ed4169263d2693fcbf9eeb84a41974548
Ben Thompson <thompson.benedictjames@gmail.com>
2017-02-12 10:00:16 +0000

    Add files via upload
    fix

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ echo -e "\e[31;43m***** Installing Drivers and Prerequisites *****\e[0m"
 sudo apt-get install software-properties-common gcc make
 sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt-get update
-sudo apt-get install ethereum ethminer geth build-essentia
+sudo apt-get install ethereum ethminer geth build-essentials
 wget http://uk.download.nvidia.com/XFree86/Linux-x86_64/375.20/NVIDIA-Linux-x86_64-375.20.run
 sudo chmod +x NVIDIA-Linux-x86_64-375.20.run
 sudo ./NVIDIA-Linux-x86_64-375.20.run --accept-license --no-questions --no-install-compat32-libs
 echo -e "\e[31;43m***** Generating Wallet: *****\e[0m"
 geth account new
-echo -e "\e[32;43m***** Done! To start mining do: 'ethminer -G -F YOURPOOL --farm-recheck 200' *****\e[0m"
+echo -e "\e[31;43m***** Done! To start mining do: 'ethminer -G -F YOURPOOL --farm-recheck 200' *****\e[0m"
