802ecba2acebbad248481b61df89f1b49c615b7a
Ben Thompson <thompson.benedictjames@gmail.com>
2017-02-12 09:55:49 +0000

    Add files via upload
    fixed problems

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 # Ethereum miner for Microsoft Azure
 # Script by Ben Thompson
 echo -e "\e[31;43m***** Installing Drivers and Prerequisites *****\e[0m"
-sudo apt-get install software-properties-common
+sudo apt-get install software-properties-common gcc make
 sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt-get update
 sudo apt-get install ethereum ethminer geth build-essentia
 wget http://uk.download.nvidia.com/XFree86/Linux-x86_64/375.20/NVIDIA-Linux-x86_64-375.20.run
 sudo chmod +x NVIDIA-Linux-x86_64-375.20.run
-sudo ./NVIDIA-Linux-x86_64-375.20.run --accept-license --no-questions --disable-nouveau --no-install-compat32-libs
+sudo ./NVIDIA-Linux-x86_64-375.20.run --accept-license --no-questions --no-install-compat32-libs
 echo -e "\e[31;43m***** Generating Wallet: *****\e[0m"
 geth account new
-echo -e "\e[31;43m***** Done! To start mining do: 'ethminer -G -F YOURPOOL --farm-recheck 200' *****\e[0m"
+echo -e "\e[32;43m***** Done! To start mining do: 'ethminer -G -F YOURPOOL --farm-recheck 200' *****\e[0m"
