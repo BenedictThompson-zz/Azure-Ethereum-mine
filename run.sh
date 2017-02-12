ca1ae9bc3a5299aad09c2252bc8264099259404a
Ben Thompson <thompson.benedictjames@gmail.com>
2017-02-12 15:30:00 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo -e "\e[31;43m***** Installing Drivers and Prerequisites *****\e[0m"
 sudo apt-get install software-properties-common gcc make
 sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt-get update
-sudo apt-get install ethereum ethminer geth build-essentials
+sudo apt-get install ethereum ethminer geth build-essential
 wget http://uk.download.nvidia.com/XFree86/Linux-x86_64/375.20/NVIDIA-Linux-x86_64-375.20.run
 sudo chmod +x NVIDIA-Linux-x86_64-375.20.run
 sudo ./NVIDIA-Linux-x86_64-375.20.run --accept-license --no-questions --no-install-compat32-libs
