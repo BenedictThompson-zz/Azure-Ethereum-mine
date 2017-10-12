bc5c3bedcac92e00d856e092b0e56c6ce9f5b788
Ben Thompson <thompson.benedictjames@gmail.com>
2017-10-12 18:51:21 +0100

    Update run.sh
    Added auto accept.

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 # Ethereum miner for Microsoft Azure
 # Script by Ben Thompson
 echo -e "\e[31;43m***** Installing Drivers and Prerequisites *****\e[0m"
-sudo apt-get install software-properties-common gcc make
+sudo apt-get install software-properties-common gcc make -y
 sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt-get update
-sudo apt-get install ethereum ethminer geth build-essential
+sudo apt-get install ethereum ethminer geth build-essential -y
 wget http://uk.download.nvidia.com/XFree86/Linux-x86_64/375.26/NVIDIA-Linux-x86_64-375.26.run
 sudo chmod +x NVIDIA-Linux-x86_64-375.26.run
 sudo ./NVIDIA-Linux-x86_64-375.26.run --accept-license --no-questions --no-install-compat32-libs
